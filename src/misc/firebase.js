65aa9b37db9a8c0238b95af02dc241e867198813
Rajni Bhatt <rajnibhatt.rb@gmail.com>
2023-07-24 19:52:17 +0530

    firebase config

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtM5wC4mhMaweWfeogtmldwZigngr7pEE",
+  authDomain: "chat-web-app-610f0.firebaseapp.com",
+  projectId: "chat-web-app-610f0",
+  storageBucket: "chat-web-app-610f0.appspot.com",
+  messagingSenderId: "298574476022",
+  appId: "1:298574476022:web:0917818919b7c8db0772d7"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
