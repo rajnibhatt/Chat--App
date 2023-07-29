2db5c24dd5d9b65f2f02ea96d0a808039b13df94
Rajni Bhatt <rajnibhatt.rb@gmail.com>
2023-07-29 18:38:19 +0530

    llinked facebook ad google in one account,created provider block

---
+++ src/Dashboard/ProviderBlock.js
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import { auth } from "../misc/firebase";
+import { Alert, Button, Icon, Tag } from "rsuite";
+import {
+    FacebookAuthProvider,
+    GoogleAuthProvider,
+    linkwithPopup,
+    unlink} from 'firebase/auth';
+
+const ProviderBlock = ()=>{
+
+    const [isConnected,setIsConnected] = 
+    useState({'google.com': auth.currentUser.providerData.some((data)=>data.providerId === 'google.com'),
+    'facebook.com': auth.currentUser.providerData.some((data)=> data.providerId === 'facebook.com')
+});
+
+const updateIsConnected = (providerId,value)=>{
+    setIsConnected(p=>{
+        return{
+            ...p,
+            [providerId]: value,
+        };
+    });
+};
+
+
+const unlink = async(ProviderId) => {
+    try{
+        if(auth.currentUser.providerData.length === 1){
+            throw new Error(`you can not disconnect from ${ProviderId.id}`)
+        }
+   await auth.currentUser.unlink(ProviderId);
+   updateIsConnected(ProviderId,false);
+    }catch(err){
+        Alert.error(err.message,4000);
+
+    }
+
+}
+
+
+const unlinkFacebook = () => {
+    unlink('facebook.com');
+}
+const unlinkGoogle = () => {
+    unlink('google.com')
+}
+
+
+const link = async (Provider)=>{
+    try{
+         await auth.currentUser.linkwithPopup(Provider)
+         Alert.info(`Linked To ${Provider.ProviderId}`,4000);
+    }catch (err) {
+       Alert.error(err.message,4000);
+       updateIsConnected(Provider.providerId,true);
+
+    }
+
+}
+
+
+const linkFacebook = () => {
+    link(new FacebookAuthProvider())
+
+};
+const linkGoogle = () => {
+    link(new GoogleAuthProvider())
+};
+
+
+    return(
+        <div>
+            {isConnected['google.com'] &&(
+            <Tag color = "green" closable onClose={unlinkGoogle}>
+                <Icon icon={"google" }>
+                    Connected
+                </Icon>
+                </Tag>
+               )}
+               {isConnected['facebook.com'] &&(
+                <Tag color = "blue" closable onClose={unlinkFacebook}>
+                <Icon icon={"facebook"}>
+                Connected
+                </Icon>
+            </Tag>
+              )}
+            <div className="mt-2">
+                {!isConnected['google.com'] &&(
+                <Button block color="green" onClick={linkGoogle}>
+                    <Icon icon="google"></Icon>
+                    Link to google
+                </Button>
+                )}
+                {!isConnected["facebook.com"] &&(
+                <Button block color="blue" onClick={linkFacebook}>
+                    <Icon icon="facebook">
+
+                    </Icon>
+                    link to facebook
+                </Button>
+                 )}
+            </div>
+        </div>
+    )
+}
+export default ProviderBlock;
--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,15 +1,23 @@
 import React from "react";
-import { Drawer,Button, Divider } from 'rsuite';
+import { Drawer,Button, Divider, Alert } from 'rsuite';
 import { useProfile } from "../Context/Profile.Context";
 import EditableInput from "../Component/EditableIInputs";
+import { database } from "../misc/firebase";
+import { ref, set } from "firebase/database";
+import ProviderBlock from "./ProviderBlock";
 
 
 const Dashboard = ({onSignOut}) => {
 const {profile} = useProfile();
 const onSave = async newData =>{
-    console.log(newData);
-
-}
+    const userNickNameRef = ref(database, `/Profiles/${profile.uid}/name`);
+    try{
+        await set(userNickNameRef, newData);
+        Alert.success('Nick name has been updated',4000);
+    }catch(err){
+        Alert.error(err.message,4000);
+    }
+};
     return (
     <>
     <Drawer.Header>
@@ -18,7 +26,9 @@ const onSave = async newData =>{
         </Drawer.Title>
     </Drawer.Header>
     <Drawer.Body>
+
         <h3>Hey,{profile.name}</h3>
+        <ProviderBlock></ProviderBlock>
         <Divider/>
         <EditableInput
         name= "NickName"
